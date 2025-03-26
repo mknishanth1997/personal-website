0c6feced99c603abc21cc13f157f4c82a19d9013
nishanth <mksabarinishanth@gmail.com>
2025-03-26 23:55:54 +0530

    bug in responsive desging bcoz of animatin, removed it from res desgin

--- header.js
+++ header.js
@@ -2,6 +2,8 @@ const hamBurger = document.querySelector('.hamburger');
 hamBurger.addEventListener('click', headerMenuShowHide);
 
 function headerMenuShowHide() {
+  console.log('Hamburger function working: header.js');
   const headerLinks = document.querySelector('.header-links');
   headerLinks.classList.toggle('active');
 }
+console.log('HI');
